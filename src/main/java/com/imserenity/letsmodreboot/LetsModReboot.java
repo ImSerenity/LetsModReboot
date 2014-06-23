fe4c31cb72d761a5e8343fc82b8e3f9d198b6f72
Thomas <tjww1995@gmail.com>
2014-06-23 13:37:20 +0100

    Basic Setup
    Basic gradle script setup.
    'eclipse' folder remade to 'idea' through setupDecompWorkspace.
    Basic Mod file created

---
+++ src/main/java/com/imserenity/letsmodreboot/LetsModReboot.java
@@ -0,0 +1,33 @@
+package com.imserenity.letsmodreboot;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by Thomas on 23/06/2014.
+ */
+
+@Mod(modid="LetsModReboot", name="LetsModReboot", version="1.7.2-1.0")
+public class LetsModReboot {
+
+    //modid parameter - tamper check
+    @Mod.Instance("LetsModReboot")
+    public static LetsModReboot instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
