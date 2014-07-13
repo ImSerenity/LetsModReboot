435b85bf2ccc4c67f55189596f8d04e976500fd2
Thomas <tjww1995@gmail.com>
2014-07-13 20:20:54 +0100

    Push for new laptop

---
+++ src/main/java/com/imserenity/letsmodreboot/init/Recipes.java
@@ -0,0 +1,19 @@
+package com.imserenity.letsmodreboot.init;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+
+/**
+ * Created by Thomas on 11/07/2014.
+ */
+public class Recipes 
+{
+    public static void init()
+    {
+        initModRecipes();
+    }
+
+    private static void initModRecipes()
+    {
+        GameRegistry.a
+    }
+}
